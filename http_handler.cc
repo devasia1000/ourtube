6f569dda8940a3c8e9444005f769422b249202c8
Devasia Manuel <devasia1000@gmail.com>
2013-08-01 09:06:40 -0400

    removed debugging statements

--- http_handler.cc
+++ http_handler.cc
@@ -77,8 +77,8 @@ void HTTPHandler::connect_to_server( void )
 
   Address server_addr( host, service );
   
-  fprintf( stderr, "Opening connection to host %s @ %s\n",
-	   host.c_str(), server_addr.str().c_str() );
+  //fprintf( stderr, "Opening connection to host %s @ %s\n",
+  //	   host.c_str(), server_addr.str().c_str() );
  
 
   server_socket_.connect( server_addr );
